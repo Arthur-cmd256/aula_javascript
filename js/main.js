6a1880b27429983a22e325bd471c0a8d10bd32d0
Arthur <arthurdelimadonascimento@gmail.com>
2021-04-11 16:07:47 -0300

    Adicionando index e pasta JS

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+var nome="Arthur Nascimento";
+var idade=19;
+var idade2=10;
+var frase="Japão é o melhor time do mundo"
+var n1=5;
+var n2=3;
+//alert("Bem-vindo "+nome+", você tem "+idade+" anos");
+//alert(idade+idade2);
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+console.log(n1*n2);
