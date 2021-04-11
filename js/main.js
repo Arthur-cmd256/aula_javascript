859937484ce993525fc24db406986807c8ced1d3
Arthur <arthurdelimadonascimento@gmail.com>
2021-04-11 16:34:09 -0300

    Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-var nome="Arthur Nascimento";
+/*var nome="Arthur Nascimento";
 var idade=19;
 var idade2=10;
 var frase="Japão é o melhor time do mundo"
@@ -13,4 +13,29 @@ console.log(frase.replace("Japão", "Brasil"));
 //alert(frase.replace("Japão", "Brasil"));
 console.log(frase.toUpperCase());
 console.log(frase.toLowerCase());
-console.log(n1*n2);
+console.log(n1*n2);*/
+
+/*var lista=["maça", "pêra", "laranja"];
+//lista.push("uva"); // Para adicionar um elemento a lista
+//lista.pop()//Tira o ultimo elemento da lista
+console.log(lista);
+console.log(lista.length);
+//console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join(" - "));
+//console.log(lista[1]);
+//alert(lista[1]);*/
+
+/*var fruta = {nome:"maça", cor:"vermelha"};
+
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxo"}];
+
+console.log(frutas);
+alert(frutas[1].nome);*/
+
