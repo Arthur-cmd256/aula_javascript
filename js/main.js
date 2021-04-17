5db1acf52f9220cc63b03950679ac50f270f2e96
Arthur <arthurdelimadonascimento@gmail.com>
2021-04-17 11:25:12 -0300

    Parte2: Manipulando elementos da página

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
+function clicou(){
+    document.getElementById("agradecimento") .innerHTML="<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"))
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://github.com/Arthur-cmd256/aula_javascript");
+    //window.location.href = "https://github.com/Arthur-cmd256/aula_javascript";
+}
+
+function trocar(elemento){
+    elemento.innerHTML="Obrigado por passar o mouse!"
+    //document.getElementById("mousemove").innerHTML="Obrigado por passar o mouse!";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML="Passe o mouse aqui"
+    //document.getElementById("mousemove").innerHTML="Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
 /*function soma(n1, n2){
     return n1+n2;
 }
